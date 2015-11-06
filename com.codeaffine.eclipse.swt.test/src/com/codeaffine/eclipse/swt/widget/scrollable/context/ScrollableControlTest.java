a038f85bd91c6df4afb1732f5763d190d2b4ddea
Frank Appel <fappel@codeaffine.com>
2015-11-06 03:42:21 +0100

    Ignore failing test on GTK

--- com.codeaffine.eclipse.swt.test/src/com/codeaffine/eclipse/swt/widget/scrollable/context/ScrollableControlTest.java
+++ com.codeaffine.eclipse.swt.test/src/com/codeaffine/eclipse/swt/widget/scrollable/context/ScrollableControlTest.java
@@ -24,14 +24,17 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import com.codeaffine.eclipse.swt.test.util.DisplayHelper;
+import com.codeaffine.eclipse.swt.test.util.SWTIgnoreConditions.GtkPlatform;
+import com.codeaffine.test.util.junit.ConditionalIgnoreRule;
+import com.codeaffine.test.util.junit.ConditionalIgnoreRule.ConditionalIgnore;
 
 public class ScrollableControlTest {
 
   private static final String KEY = "key";
   private static final Object VALUE = new Object();
 
-  @Rule
-  public final DisplayHelper displayHelper = new DisplayHelper();
+  @Rule public final ConditionalIgnoreRule conditionalIgnoreRule = new ConditionalIgnoreRule();
+  @Rule public final DisplayHelper displayHelper = new DisplayHelper();
 
   @Test
   public void getItemHeightOfTable() {
@@ -387,6 +390,7 @@ public class ScrollableControlTest {
   }
 
   @Test
+  @ConditionalIgnore( condition = GtkPlatform.class )
   public void getVerticalBarSelection() {
     Tree scrollable = ( Tree )createScrollableWithVisibleScrollbars();
     ScrollableControl<?> scrollableControl = new ScrollableControl<>( scrollable );
