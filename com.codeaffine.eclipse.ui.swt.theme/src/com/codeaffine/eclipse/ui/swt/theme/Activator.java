eb5b78adf28a58ffaab7a845143ccdc52884a741
Frank Appel <fappel@codeaffine.com>
2016-02-18 12:55:40 +0100

    Rewind the changes for #57 as they didn't work

--- com.codeaffine.eclipse.ui.swt.theme/src/com/codeaffine/eclipse/ui/swt/theme/Activator.java
+++ com.codeaffine.eclipse.ui.swt.theme/src/com/codeaffine/eclipse/ui/swt/theme/Activator.java
@@ -1,5 +1,7 @@
 package com.codeaffine.eclipse.ui.swt.theme;
 
+import static com.codeaffine.eclipse.ui.swt.theme.FontLoader.FONTS_DIRECTORY;
+
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -8,13 +10,17 @@ public class Activator extends AbstractUIPlugin {
   private static Activator instance;
 
   private final ScrollbarPreferenceApplicator preferenceApplictor;
+  private final FontLoader fontLoader;
 
   public Activator() {
+    fontLoader = new FontLoader( FONTS_DIRECTORY );
+    new FontRegistryUpdater();
     preferenceApplictor = new ScrollbarPreferenceApplicator();
   }
 
   @Override
   public void start( BundleContext context ) throws Exception {
+    new Thread( () -> fontLoader.load( context ) ).start();
     getPreferenceStore().addPropertyChangeListener( preferenceApplictor );
     instance = this;
   }
