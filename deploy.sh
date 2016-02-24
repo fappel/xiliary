01b788580dbf1a5bbba65e733ec7753b3172b761
Frank Appel <fappel@codeaffine.com>
2016-02-24 11:27:46 +0100

    Provide composite p2 repository (work in progress, part I)

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function error_exit
+{
+  echo -e "\e[01;31m$1\e[00m" 1>&2
+  exit 1
+}
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && ( [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "development" ]); then
+  echo -e "Starting to deploy to gh-pages\n"
+
+  # cd into temporary deployment work directory
+  cd deployment-work
+  
+  echo -e "Repository Content\n"
+  ls
+  cd development
+  echo -e "\nDevelopment Directory\n"
+  ls
+  cd ..
+
+  # add, commit and push files
+#  git add -f -A
+#  git commit -m "[ci skip] Deploy Travis build #$TRAVIS_BUILD_NUMBER to gh-pages"
+#  git push -fq origin gh-pages > /dev/null 2>&1 || error_exit "Error uploading the build result to gh-pages"
+
+  # go back to the directory where we started
+  cd ..
+
+  echo -e "Done with deployment to gh-pages\n"
+fi
---
+++ prepare.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function error_exit
+{
+  echo -e "\e[01;31m$1\e[00m" 1>&2
+  exit 1
+}
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && ( [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "development" ]); then
+  echo -e "Checkout composite repository from gh-pages\n"
+
+  # create and cd into temporary deployment work directory
+  mkdir deployment-work
+  cd deployment-work
+
+  # setup git and clone from gh-pages branch
+  git config --global user.email "travis-deployer@codeaffine.com"
+  git config --global user.name "Travis Deployer"
+  git clone --quiet --branch=gh-pages https://fappel:${GH_TOKEN}@github.com/fappel/xiliary.git . > /dev/null 2>&1 || error_exit "Error cloning gh-pages"
+
+  # remove web content
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
+    export DEPLOY_WORK_DIRECTORY="$PWD"
+    rm *.html
+    rm *.jar
+    rm -rf ./plugins
+    rm -rf ./features
+    rm -rf ./assets
+    rm -rf ./images
+    rm -rf ./development
+  fi
+  if [ "$TRAVIS_BRANCH" == "development" ]; then
+    cd development
+    export DEPLOY_WORK_DIRECTORY="$PWD/development"
+    rm *.html
+    rm *.jar
+    rm -rf ./plugins
+    rm -rf ./features
+    rm -rf ./assets
+    rm -rf ./images
+    rm -rf ./development
+    cd ..
+  fi
+
+  # go back to the directory where we started
+  cd ..
+
+  echo -e "Done with composite repository deployment preparations\n"
+fi
