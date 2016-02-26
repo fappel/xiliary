b87b466fa316cff0f6cd2b89084279d292c907b8
Frank Appel <fappel@codeaffine.com>
2016-02-26 11:05:17 +0100

    Correction of license header cudos

--- com.codeaffine.test.util/src/com/codeaffine/test/util/lang/EqualsTester.java
+++ com.codeaffine.test.util/src/com/codeaffine/test/util/lang/EqualsTester.java
@@ -1,12 +1,12 @@
 /**
- * Copyright (c) 2014 - 2016 Frank Appel
+ * Copyright (c) 2014 - 2016 Rüdiger Herrmann
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Frank Appel - initial API and implementation
+ *   Rüdiger Herrmann - initial API and implementation
  */
 package com.codeaffine.test.util.lang;
 
