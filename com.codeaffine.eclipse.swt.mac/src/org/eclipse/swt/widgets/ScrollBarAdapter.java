8efc4cc8b4b6c04489df9876a570d780b2a453e8
Frank Appel <fappel@codeaffine.com>
2016-03-03 09:33:46 +0100

    Next attempt on fragment and feature platform dependency declarations

---
+++ com.codeaffine.eclipse.swt.mac/src/org/eclipse/swt/widgets/ScrollBarAdapter.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2014 - 2016 Frank Appel
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Frank Appel - initial API and implementation
+ */
+package org.eclipse.swt.widgets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+
+import com.codeaffine.eclipse.swt.widget.scrollbar.FlatScrollBar;
+
+public class ScrollBarAdapter extends ScrollBar {
+
+  private FlatScrollBar scrollBar;
+
+  private ScrollBarAdapter() {
+    super( null, -1 );
+  }
+
+  public void setScrollbar( FlatScrollBar scrollBar ) {
+    this.scrollBar = scrollBar;
+  }
+
+  @Override
+  public Point getSize() {
+    return scrollBar.getSize();
+  }
+
+  @Override
+  public void setSelection( int selection ) {
+    scrollBar.setSelection( selection );
+    scrollBar.notifyListeners( SWT.None );
+  }
+
+  @Override
+  public int getSelection() {
+    return scrollBar.getSelection();
+  }
+
+  @Override
+  public void addListener( int eventType, Listener listener ) {
+    if( eventType == SWT.Selection ) {
+      scrollBar.addListener( eventType, listener );
+    } else {
+      super.addListener( eventType, listener );
+    }
+  }
+
+  @Override
+  public boolean isVisible() {
+    return scrollBar.isVisible();
+  }
+}
