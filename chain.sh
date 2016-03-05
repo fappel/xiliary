7cb3ba7360a33872d2a2e3e47a9679fc26591b11
Frank Appel <fappel@codeaffine.com>
2016-03-05 14:45:02 +0100

    Configure global git repo email and name settings in chain.sh

--- chain.sh
+++ chain.sh
@@ -11,17 +11,25 @@ set -e
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && ( [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "development" ]); then
   # trigger OS-X-build
   echo -e "Trigger OS X build with content from $TRAVIS_BRANCH\n"
-  git checkout "$TRAVIS_BRANCH"
   
+  # setup git and clone from gh-pages branch
+  git checkout "$TRAVIS_BRANCH"
+  git config --global user.email "travis-deployer@codeaffine.com"
+  git config --global user.name "Travis Deployer"
+
+  # prepare OS X build configuration  
   rm .travis.yml
   mv .travis-os-x.yml .travis.yml
   touch "$TRAVIS_BRANCH"
   git add -A
   git commit -m "Update of OS-X-build branch with latest from $TRAVIS_BRANCH"
   
+  # push OS X build configuration commit to OS-X-build brach
   git remote set-url origin https://fappel:${GH_TOKEN}@github.com/fappel/xiliary.git
   git config --global push.default simple
   git push origin HEAD:OS-X-build -f
+  
+  # clear user/password info
   git remote set-url origin https://xxx:xxx@github.com/fappel/xiliary.git
   echo -e "Done with OS X build trigger\n"
 fi
