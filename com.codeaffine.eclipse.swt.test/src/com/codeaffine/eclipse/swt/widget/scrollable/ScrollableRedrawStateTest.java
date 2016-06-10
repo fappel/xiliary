a141ab89d7ec34a11131d28be8dc2085a52c1248
Frank Appel <fappel@codeaffine.com>
2016-06-10 10:55:44 +0200

    Add missing license header of ScrollableRedrawStateTest

--- com.codeaffine.eclipse.swt.test/src/com/codeaffine/eclipse/swt/widget/scrollable/ScrollableRedrawStateTest.java
+++ com.codeaffine.eclipse.swt.test/src/com/codeaffine/eclipse/swt/widget/scrollable/ScrollableRedrawStateTest.java
@@ -11,6 +11,16 @@ import org.junit.Test;
 
 import com.codeaffine.eclipse.swt.widget.scrollable.context.ScrollableControl;
 
+/**
+ * Copyright (c) 2014 - 2016 Frank Appel
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Frank Appel - initial API and implementation
+ */
 public class ScrollableRedrawStateTest {
 
   private ScrollableRedrawState redrawState;
