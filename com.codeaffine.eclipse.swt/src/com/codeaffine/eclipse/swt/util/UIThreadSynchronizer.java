72d713156a7a7107beb672167d378fd03cfc1560
FrankAppel <FrankAppel@192.168.0.23>
2017-01-09 12:59:46 +0100

    Adjust license header check for UIThreadSynchronizer

--- com.codeaffine.eclipse.swt/src/com/codeaffine/eclipse/swt/util/UIThreadSynchronizer.java
+++ com.codeaffine.eclipse.swt/src/com/codeaffine/eclipse/swt/util/UIThreadSynchronizer.java
@@ -1,4 +1,4 @@
-/**
+/*******************************************************************************
  * Copyright (c) 2014 - 2017 Rüdiger Herrmann
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *   Rüdiger Herrmann - initial API and implementation
- */
+ ******************************************************************************/
 package com.codeaffine.eclipse.swt.util;
 
 import org.eclipse.swt.SWT;
