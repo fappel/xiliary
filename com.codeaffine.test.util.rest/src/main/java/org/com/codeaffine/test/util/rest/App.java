0d9d3692b453dc21bc4fee505cb3b1e9c19bab47
Frank Appel <fappel@codeaffine.com>
2018-11-18 11:01:13 +0100

    chore: add automatic module header to manifests

---
+++ com.codeaffine.test.util.rest/src/main/java/org/com/codeaffine/test/util/rest/App.java
@@ -0,0 +1,13 @@
+package org.com.codeaffine.test.util.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
