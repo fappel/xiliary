111a616ac5d0a30d0db34664f21ac0563700e5d8
Frank Appel <fappel@codeaffine.com>
2018-11-20 05:50:34 +0100

    chore: Added missing licence headers

--- com.codeaffine.util.test/src/com/codeaffine/util/EnumSwitchTest.java
+++ com.codeaffine.util.test/src/com/codeaffine/util/EnumSwitchTest.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2014 - 2017 Frank Appel
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Frank Appel - initial API and implementation
+ */
 package com.codeaffine.util;
 
 import static com.codeaffine.util.EnumSwitchTest.EnumWithTwoElements.ONE;
