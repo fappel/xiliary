1ac0636de71e4a01539f329a6c1fcd87b1288c03
Frank Appel <fappel@codeaffine.com>
2018-11-20 05:08:18 +0100

    chore: introduction JUnit 5

--- com.codeaffine.test.util.rest/src/test/java/org/com/codeaffine/test/util/rest/AppTest.java
+++ com.codeaffine.test.util.rest/src/test/java/org/com/codeaffine/test/util/rest/AppTest.java
@@ -1,38 +1,13 @@
 package org.com.codeaffine.test.util.rest;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.jupiter.api.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+  @Test
+  void firstTest() {
+    assertThat( 1+1 ).isEqualTo( 2 );
+  }
 }
