78bc78871656dbe6f139e662a8a195f8f499b438
Frank Appel <fappel@codeaffine.com>
2021-12-12 10:29:00 +0100

    update licence header text II

--- com.codeaffine.eclipse.swt.test/src/com/codeaffine/eclipse/swt/widget/navigationbar/NavigationItemControllerStub.java
+++ com.codeaffine.eclipse.swt.test/src/com/codeaffine/eclipse/swt/widget/navigationbar/NavigationItemControllerStub.java
@@ -1,10 +1,10 @@
 /**
- * Copyright (c) 2014 - 2017 Frank Appel
+ * Copyright (c) 2014 - 2022 Frank Appel
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *   Frank Appel - initial API and implementation
  */
